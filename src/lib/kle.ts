f29cb638eb2b9d0f00b48d8c59da0969ab9ccf39
i7d3v3l0p3r <i7d3v3l0p3r@yandex.ru>
2021-11-08 10:46:01 +0000

    #5 Moved parser logic to independent file

--- src/lib/kle.ts
+++ src/lib/kle.ts
@@ -24,3 +24,73 @@ export interface KLEKey {
     n?: boolean; // homing bar
     a?: number; // legends align
 }
+
+export interface ParsedKLEKey {
+    position: { x: number; y: number };
+    size: { width: number; height: number };
+    pivot: { x: number; y: number };
+    angle: number;
+}
+
+export function* parseData(rows: KLERows): Generator<ParsedKLEKey, any, ParsedKLEKey> {
+    const keyState: KLEKey = { x: 0, y: 0, rx: 0, ry: 0, h: 1, w: 1 };
+      const cluster = { x: 0, y: 0 };
+      for (const row of rows) {
+        if (!Array.isArray(row)) {
+          // TODO: Parse metadata
+          continue;
+        }
+        for (const key of row) {
+          if (typeof key === 'object') {
+            if ('rx' in key) {
+              keyState.rx = cluster.x = key.rx;
+              keyState.x = cluster.x;
+              keyState.y = cluster.y;
+            }
+
+            if ('ry' in key) {
+              keyState.ry = cluster.y = key.ry;
+              keyState.x = cluster.x;
+              keyState.y = cluster.y;
+            }
+
+            if (key.x) keyState.x += key.x;
+            if (key.y) keyState.y += key.y;
+            if ('r' in key) keyState.r = key.r;
+            if ('w' in key) keyState.w = key.w;
+            if ('h' in key) keyState.h = key.h;
+
+            if ('h2' in key && !('h' in key)) keyState.h = key.h2;
+            else if ('h2' in key) keyState.h2 = key.h2;
+
+            if ('w2' in key && !('w' in key)) keyState.w = key.w2;
+            else if ('w2' in key) keyState.w2 = key.w2;
+          } else {
+            const position = {
+              x: keyState.x,
+              y: keyState.y,
+            };
+
+            const size = {
+              width: keyState.w || keyState.w2 || 1,
+              height: keyState.h || keyState.h2 || 1,
+            };
+
+            const pivot = {
+              x: keyState.rx ?? 0,
+              y: keyState.ry ?? 0,
+            };
+
+            const angle = keyState.r ?? 0;
+
+            yield { position, size, pivot, angle };
+            
+            keyState.x += size.width;
+            keyState.w = keyState.h = 1;
+            keyState.x2 = keyState.y2 = keyState.w2 = keyState.h2 = 0;
+          }
+        }
+        ++keyState.y;
+        keyState.x = keyState.rx;
+    }
+}
