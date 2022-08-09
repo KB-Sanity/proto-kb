b26ad5058ce404a6d59ac55f8ca59087071363da
i7d3v3l0p3r <i7d3v3l0p3r@yandex.ru>
2022-08-10 00:05:31 +0300

    Update types

---
+++ src/types.ts
@@ -0,0 +1,27 @@
+export * from './interfaces';
+
+export * from './api/index';
+export * from './api/utils';
+
+export * from './components/LayoutEditor/interfaces';
+export * from './components/Sidebar/interfaces';
+export * from './components/Sidebar/constants';
+export * from './components/Toolbar/interfaces';
+
+export * from './core/http/index';
+export * from './core/Events';
+export * from './core/Keyboard';
+export * from './core/KeyCap';
+export * from './core/PluginsLoader';
+export * from './core/ProtoKBApplication';
+export * from './core/ProtoPlugin';
+
+export * from './lib/colorTools';
+export * from './lib/file';
+
+export * from './plugins/BaseTools/index';
+export * from './plugins/ImportExport/index';
+export * from './plugins/KeyParameters/index';
+export * from './plugins/index';
+
+export * from './store/index';
